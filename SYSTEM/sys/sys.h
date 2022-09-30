5a8b637df43e39e00234fd77c67c522a892c2ef0
1cwb <1090378278@qq.com>
2022-09-30 15:15:32 +0800

    update ld

--- SYSTEM/sys/sys.h
+++ SYSTEM/sys/sys.h
@@ -37,6 +37,15 @@
 #define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)
 #define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n) 
 
+#define DTCM_MEM   __attribute__((section(".dtcmram")))
+#define DTCM_MEM_ALIGN(x)   __attribute__((section(".dtcmram"))) __attribute__((aligned(x)))
+
+#define D2_MEM   __attribute__((section(".ramd2")))
+#define D2_MEM_ALIGN(x)   __attribute__((section(".ramd2"))) __attribute__((aligned(x)))
+
+#define D3_MEM   __attribute__((section(".ramd3")))
+#define D3_MEM_ALIGN(x)   __attribute__((section(".ramd3"))) __attribute__((aligned(x)))
+
 HAL_StatusTypeDef Stm32_Clock_Init(uint32_t pllm, uint32_t plln, uint32_t pllp, uint32_t pllq);
 
 #ifdef __cplusplus
